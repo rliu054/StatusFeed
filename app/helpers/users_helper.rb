df85218a1c58e3d5913f4536c3e347fd28096891
Rui <rliu054@uottawa.ca>
2016-09-18 15:05:16 -0400

    tweak users helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,10 @@
 module UsersHelper
   def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    # http://s.gravatar.com/avatar/c6b2687fb593958bda5531ebaaf483c2?s=80
+    gravatar_url = "http://s.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    # gravatar_url = "https://cdn4.iconfinder.com/data/icons/new-google-logo-2015/400/new-google-favicon-128.png"
+    # image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
