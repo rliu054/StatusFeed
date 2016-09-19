ea9eb6272b08599cac75a2e8ad33cb0efc9963ad
Rui <rliu054@uottawa.ca>
2016-09-19 19:05:36 -0400

    Finish implementing log in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,7 @@
 module UsersHelper
   def gravatar_for(user, size: 80)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    # http://s.gravatar.com/avatar/c6b2687fb593958bda5531ebaaf483c2?s=80
     gravatar_url = "http://s.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
-    # gravatar_url = "https://cdn4.iconfinder.com/data/icons/new-google-logo-2015/400/new-google-favicon-128.png"
-    # image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,14 @@ class User < ApplicationRecord
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\w\.[a-z]+\z/i
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
