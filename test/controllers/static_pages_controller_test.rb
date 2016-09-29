220481ab70a4cc36daa7f9cb96f66ce42538c5c8
Rui <iamliurui@gmail.com>
2016-09-28 21:33:03 -0400

    Use my name

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
+    @base_title = "Rui Liu's first Rails app"
   end
 
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", @base_title
   end
 
   test "should get help" do
